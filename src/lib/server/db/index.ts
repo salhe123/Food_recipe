cba7a66163ab5b3ae52ddb81a720f069f18aa0fa
salhe123 <your-github-email@example.com>
2025-10-22 22:41:50 +0300

    env update

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -2,10 +2,11 @@ import { drizzle } from 'drizzle-orm/mysql2';
 import { createPool } from 'mysql2/promise';
 import * as schema from '$lib/server/db/schema';
 
-import { env } from '$env/dynamic/private';
-
 const pool = createPool({
-  uri: env.DATABASE_URL || 'mysql://root:123456@localhost:3306/recipe_app',
+  host: 'localhost',
+  user: 'root',
+  password: '123456',
+  database: 'recipe_app',
 });
 
 export const db = drizzle(pool, { schema, mode: 'default' });
