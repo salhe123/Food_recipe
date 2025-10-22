aeac633d9bb0bfca1ee066f6031879fc075299a5
salhe123 <your-github-email@example.com>
2025-10-22 22:35:31 +0300

    updated done

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,6 +1,7 @@
 import { drizzle } from 'drizzle-orm/mysql2';
 import { createPool } from 'mysql2/promise';
 import * as schema from '$lib/server/db/schema';
+
 import { env } from '$env/dynamic/private';
 
 const pool = createPool({
